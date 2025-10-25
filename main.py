5c93ee119a7dcf00f3cba7046b32c56bf3d569f8
ArtyomBataninSuper228 <artyom.batanin3331@gmail.com>
2025-10-25 12:51:25 +0500

    commit

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import pygame
+import telebot
 from telebot import types
+
+bot = telebot.TeleBot('BOT-TOKEN')
+
+@bot.message_handler(commands = ['start'])
+def url(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text='Наш сайт', url='https://habr.com/ru/all/')
+    bot.send_photo(message.from_user.id, photo=open('2025-10-25 12.50.58.jpg', 'rb'))
+    markup.add(btn1)
+    bot.send_message(message.from_user.id, "По кнопке ниже можно перейти на сайт хабра", reply_markup = markup)
